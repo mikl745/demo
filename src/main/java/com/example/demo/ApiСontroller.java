9f1f7d9001567ab9f2d2f9990326cc8e4f032e27
mikl745 <mikl745@yandex.ru>
2021-11-16 23:22:35 +0300

    first

---
+++ src/main/java/com/example/demo/ApiСontroller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+@RestController
+class ApiController {
+    @GetMapping("books")
+    public ResponseEntity<String> getText() {
+        return ResponseEntity.ok("Hello text");
+    }
+}
